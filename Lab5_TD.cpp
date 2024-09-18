134ed0c5d85c4eb32094113b964a183af7a6d752
tdoshi123 <168225392+tdoshi123@users.noreply.github.com>
2024-09-18 13:50:42 -0400

    Update Lab5_TD.cpp

--- Lab5_TD.cpp
+++ Lab5_TD.cpp
@@ -1,20 +1,37 @@
-// Lab5_TD.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Lab5_TD.cpp : This file contains the 'main' function. Program execution begins and ends there. //
 
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+char cypherChar(char ch, const vector<char>& cypher) {
+    if (ch >= 65 && ch <= 90) {
+        return cypher[ch - 65];
+    } 
+    else if (ch >= 97 && ch <= 122) {
+        char upperChar = ch - 32;
+        return cypher[upperChar - 65] + 32;
+    } 
+    else {
+        return ch;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
+    vector<char> cypher = {'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E'};
+    
+    string text;
+    cout << "Enter a string (up to 128 characters): ";
+    getline(cin, text);
+    
+    string result = "";
+    for (char ch : text) {
+        result += cypherChar(ch, cypher);
+    }
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cout << "Encoded Message: \"" << result << "\"" << endl;
+    
+    return 0;
+}
